00464397a4d2cc04e18202b1418477bf993c2e30
Myhailo Riznyk <myhailo@MBP-Myhailo.lan>
2023-11-05 17:49:42 +0200

    commit

--- client.py
+++ client.py
@@ -58,9 +58,9 @@ async def copy_messages(client: TelegramClient):
                         user_id = int(id)
                         target = await Blacklist.get_by_id(user_id)
                         if id == user_ids[0]:
-                            messages = list(filter(lambda m: m.grouped_id == message.grouped_id, target_messages))
-                            message = (await client.forward_messages(main_chat, messages = messages))[0]
                             if not target:
+                                messages = list(filter(lambda m: m.grouped_id == message.grouped_id, target_messages))
+                                message = (await client.forward_messages(main_chat, messages = messages))[0]
                                 await Blacklist.add(user_id, "", f"https://t.me/c/{message.peer_id.channel_id}/{message.id}")
                     await asyncio.sleep(randint(60, 180))
             except Exception as e:
